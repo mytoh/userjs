7e7c70a53d50a17d3986d519a5421c05ca22c0f0
mytoh <mytoh@gmail.com>
2012-09-21 07:47:29 +0900

    fix gauche-ref.user.js

--- gauche-ref.user.js
+++ gauche-ref.user.js
@@ -9,25 +9,27 @@ var cssJsonString = {
   'body': {
     'color': '#aaaaaa !important',
     'background-color': '#121212 !important'
-  }
+  },
 
   'a': {
     'color': '#484878 !important'
-  }
+  },
 
   /*Function: funcname arg1 arg2 */
   /* Function: */
   'dl dt u': {
     'color': '#333358 !important'
-  }
+  },
+
   /* funcname */
   'dl dt b': {
     'color': '#aaaafa !important'
-  }
+  },
+
   /* arg1 arg2 */
   'dl dt i': {
     'color': '#555577 !important'
-  }
+  },
 
   /* code examples */
   '.example': {
