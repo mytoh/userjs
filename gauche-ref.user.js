61624e95102251e3af36a019322b0fa427489db3
mytoh <mytoh@gmail.com>
2012-09-21 07:44:27 +0900

    add script for gauche reference

---
+++ gauche-ref.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name           futaba-catalog
+// @include        http://www.callcc.net/gauche/refj/
+// @include        http://practical-scheme.net/gauche/man/
+// @require        lib/cssjson.js
+// ==/UserScript==
+
+var cssJsonString = {
+  'body': {
+    'color': '#aaaaaa !important',
+    'background-color': '#121212 !important'
+  }
+
+  'a': {
+    'color': '#484878 !important'
+  }
+
+  /*Function: funcname arg1 arg2 */
+  /* Function: */
+  'dl dt u': {
+    'color': '#333358 !important'
+  }
+  /* funcname */
+  'dl dt b': {
+    'color': '#aaaafa !important'
+  }
+  /* arg1 arg2 */
+  'dl dt i': {
+    'color': '#555577 !important'
+  }
+
+  /* code examples */
+  '.example': {
+    'background-color': '#212121',
+    'border-radius': '10px'
+  }
+};
+
+
+
+function main() {
+  CssJson.addCssJsonStyle(cssJsonString);
+}
+
+main();
