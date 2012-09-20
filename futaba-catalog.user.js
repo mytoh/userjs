487ec2971ee920a50573cc0e9797969a6e4bf98f
mytoh <mytoh@gmail.com>
2012-09-20 10:25:02 +0900

    added cssjson.js

--- futaba-catalog.user.js
+++ futaba-catalog.user.js
@@ -2,36 +2,9 @@
 // @name           futaba-catalog
 // @include        http://jun.2chan.net/b/*
 // @include        http://may.2chan.net/b/*
+// @require        /cssjson.js
 // ==/UserScript==
 
-
-function addGlobalStyle(/* String */ styles) {
-  //http://www.userscritps.org/scriptts/review/55433
-  var oStyle = document.createElement("style");
-  oStyle.setAttribute("type", "text\/css");
-  oStyle.appendChild(document.createTextNode(styles));
-  document.getElementsByTagName("head")[0].appendChild(oStyle);
-}
-
-function addCssJsonStyle(cssjson) {
-// http://www.featureblend.com/css-json.html
-  var styleStr = '';
-  for (var i in cssjson) {
-    styleStr += i + ' {\n';
-      for (var j in cssjson[i]) {
-        if (j=='CSSJSON-INHERIT-SELECTOR') {
-          for (var k in cssjson[cssjson[i][j]]) {
-            styleStr += '    ' + k + ':' + cssjson[cssjson[i][j]][k] + ';\n';
-          }
-        } else {
-          styleStr += '    ' + j + ':' + cssjson[i][j] + ';\n';
-        }
-      }
-    styleStr += "}\n";
-    }
-    addGlobalStyle(styleStr);
-    console.log(styleStr);
-}
 
 
 var cssJsonString = {
