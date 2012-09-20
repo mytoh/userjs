bef8aa33dc2101671adb01e8f3a2daceb0b55a15
mytoh <mytoh@gmail.com>
2012-09-20 20:28:03 +0900

    fix

--- futaba-catalog.user.js
+++ futaba-catalog.user.js
@@ -62,8 +62,9 @@ function addThreadId() {
   var tds = document.getElementsByTagName('td');
   for (var i in tds) {
     var threadId = document.createElement('span');
+    var child = tds[i].childNodes[0];
     threadId.setAttribute('class', 'threadid');
-    threadId.innerHTML = ':' + tds[i].childNodes[0].getAttribute('href').match(/\d+/);
+    threadId.innerHTML = ':' + child.getAttribute('href').match(/\d+/);
     tds[i].appendChild(threadId);
   }
 
