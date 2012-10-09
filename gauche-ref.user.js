1445403b7241c87823db1c850e105ebf76255ef6
mytoh <mytoh@gmail.com>
2012-10-09 22:03:06 +0900

    change colours

--- gauche-ref.user.js
+++ gauche-ref.user.js
@@ -12,18 +12,18 @@ var cssJsonString = {
   },
 
   'a': {
-    'color': '#484878 !important'
+    'color': '#204a87 !important'
   },
 
   /*Function: funcname arg1 arg2 */
   /* Function: */
   'dl dt u': {
-    'color': '#333358 !important'
+    'color': '#49483e !important'
   },
 
   /* funcname */
   'dl dt b': {
-    'color': '#aa5a3a !important'
+    'color': '#fd971f !important'
   },
 
   /* arg1 arg2 */
@@ -46,3 +46,13 @@ function main() {
 }
 
 main();
+
+// monokai colors
+//  #ae81ff
+//   #49483e
+//  #e6db74
+//  #a6e22e
+//  #66d9ef
+//  #fd971f
+//  #f92672
+ // #204a87
